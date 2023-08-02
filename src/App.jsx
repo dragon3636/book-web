e66c5a546236abdea7fb999c1574f2367964e14b
hung10t3 <hung10t@gmail.com>
2023-08-02 16:31:28 +0700

    using useLayoutEffect

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,20 @@ import './App.css'
 // import CounDown from './components/f8/CounDown';
 // import CounDownTimeout from './components/f8/CounDownTimeout';
 // import PreviewAvatar from './components/f8/PreviewAvatar';
-import Comment from './components/f8/Comment';
+import Layer from './components/f8/Layer';
 // import Dropdown from "./components/dropdown/Dropdown";
 // import Select from './components/dropdown/Select';
 // import Item from './components/dropdown/Item';
 // import List from './components/dropdown/List';
 // import { DropdownProvider } from './components/dropdown/dropdown-context';
-const countryData = ['Vietname', 'Thailand', 'China', 'Japan'];
+// const countryData = ['Vietname', 'Thailand', 'China', 'Japan'];
 
 function App() {
   const [toggle, setToggle] = useState(false);
   return (
     <div className='w-full h-96'>
       <button type="button" className='bg-orange-500 rounded font-bold text-white p-3' onClick={() => setToggle(!toggle)}>Toggle</button>
-      {toggle && <Comment></Comment>
+      {toggle && <Layer></Layer>
       }
     </div>
   )
