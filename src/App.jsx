de3a11ea49b89931f2bbb7e66ce8c19f299539f2
hung10t3 <hung10t@gmail.com>
2023-08-02 19:47:47 +0700

    using use Ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 // import List from './components/f8/List';
 // import Content from './components/f8/Content';
-// import CounDown from './components/f8/CounDown';
+import CounDown from "./components/f8/CounDown";
 // import CounDownTimeout from './components/f8/CounDownTimeout';
 // import PreviewAvatar from './components/f8/PreviewAvatar';
-import Layer from './components/f8/Layer';
+// import Layer from "./components/f8/Layer";
 // import Dropdown from "./components/dropdown/Dropdown";
 // import Select from './components/dropdown/Select';
 // import Item from './components/dropdown/Item';
@@ -16,12 +16,17 @@ import Layer from './components/f8/Layer';
 function App() {
   const [toggle, setToggle] = useState(false);
   return (
-    <div className='w-full h-96'>
-      <button type="button" className='bg-orange-500 rounded font-bold text-white p-3' onClick={() => setToggle(!toggle)}>Toggle</button>
-      {toggle && <Layer></Layer>
-      }
+    <div className="w-full h-96">
+      <button
+        type="button"
+        className="bg-orange-500 rounded font-bold text-white p-3"
+        onClick={() => setToggle(!toggle)}
+      >
+        Toggle
+      </button>
+      {toggle && <CounDown></CounDown>}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/f8/CounDown.jsx
+++ src/components/f8/CounDown.jsx
@@ -1,18 +1,49 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from "react";
 
 const CounDown = () => {
-    const [count, setCount] = useState(180);
-    useEffect(() => {
-        const timeId = setInterval(() => {
-            setCount(preValue => preValue - 1)
-        }, 1000)
-        return () => clearInterval(timeId);
-    }, [])
-    return (
-        <div>
-            <h3>{count}</h3>
-        </div>
+  const [count, setCount] = useState(180);
+  const prevCount = useRef();
+  useEffect(() => {
+    prevCount.current = count;
+    console.log(
+      "🚀 ~ file: CounDown.jsx:8 ~ useEffect ~ prevCount.current:",
+      prevCount.current
     );
+  }, [count]);
+  const timeId = useRef();
+
+  const handleUpCount = () => {
+    timeId.current = setInterval(() => {
+      setCount((preValue) => preValue - 1);
+    }, 1000);
+  };
+  const handleDownCount = () => {
+    clearInterval(timeId.current);
+  };
+  console.log(
+    "🚀 ~ file: CounDown.jsx:24 count, prevCount:",
+    count,
+    prevCount.current
+  );
+  return (
+    <div>
+      <h3>{count}</h3>
+      <button
+        type="button"
+        onClick={handleUpCount}
+        className="bg-red-500 text-white rounded-xl m-4 px-5 cursor-pointer hover:bg-red-700"
+      >
+        Start
+      </button>
+      <button
+        type="button"
+        onClick={handleDownCount}
+        className="bg-yellow-500 text-white rounded-xl m-4 px-5 cursor-pointer"
+      >
+        Stop
+      </button>
+    </div>
+  );
 };
 
 export default CounDown;
